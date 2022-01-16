c6fa57d902ca4de399ef66bae31abc6bf668facb
000hen <42735043+000hen@users.noreply.github.com>
2022-01-16 09:56:02 +0800

    Change a little things

--- notification.js
+++ notification.js
@@ -1,9 +1,12 @@
 const { Notification } = require("electron");
+const path = require("path");
 
 function sendNotification(title, body, callback) {
     var n = new Notification({
         title: title,
-        body: body
+        body: body,
+        icon: path.join(__dirname, "icons", "foodpanda.png"),
+        silent: true
     });
     n.on('click', callback);
     n.show();
